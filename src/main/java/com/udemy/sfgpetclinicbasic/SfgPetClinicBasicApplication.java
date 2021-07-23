937b67898d057ac443b15be636c951976ebe2759
Pranitapd <49433885+Pranitapd@users.noreply.github.com>
2021-07-23 18:13:28 +0530

    initial commit

---
+++ src/main/java/com/udemy/sfgpetclinicbasic/SfgPetClinicBasicApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.sfgpetclinicbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicBasicApplication.class, args);
+    }
+
+}
