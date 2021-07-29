f785afdb607ba43fdf48e90d7dc3040974f80979
Pranitapd <49433885+Pranitapd@users.noreply.github.com>
2021-07-29 08:59:51 +0530

    banner image,OwnerServiceMap,PetServiceMap,VetServiceMap

---
+++ pet-clinic-data/src/main/java/com/udemy/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,28 @@
+package com.udemy.sfgpetclinic.services.map;
+
+import com.udemy.sfgpetclinic.model.Vet;
+import com.udemy.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
