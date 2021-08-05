5f76c6b63936fa72cd953ee0c4ca1152ee2bb7a8
Pranitapd <49433885+Pranitapd@users.noreply.github.com>
2021-08-05 14:48:05 +0530

    owner contact info

--- pet-clinic-data/src/main/java/com/udemy/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/udemy/sfgpetclinic/model/Owner.java
@@ -1,5 +1,44 @@
 package com.udemy.sfgpetclinic.model;
 
-public class Owner extends Person{
+import java.util.Set;
 
+public class Owner extends Person{
+        private String address;
+        private String city;
+        private String Telephone;
+
+        private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
     }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        Telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+}
+
