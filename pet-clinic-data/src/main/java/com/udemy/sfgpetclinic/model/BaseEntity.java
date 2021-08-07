63b00f80e30f11a59f8d38dc267c071c60f6c0b8
Pranitapd <49433885+Pranitapd@users.noreply.github.com>
2021-08-07 09:14:23 +0530

    mappedsuperclass baseEntity class

--- pet-clinic-data/src/main/java/com/udemy/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/udemy/sfgpetclinic/model/BaseEntity.java
@@ -3,10 +3,13 @@ package com.udemy.sfgpetclinic.model;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;        //hibernate recommends to use box Long instead of long
 
     public Long getId() {
